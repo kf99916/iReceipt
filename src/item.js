342f0c45999099d4235016ad0cc27e2c3024ddfc
Zheng-Xiang Ke <kf99916@gmail.com>
2017-12-11 15:57:57 +0800

    Item constructor

--- src/item.js
+++ src/item.js
@@ -1,6 +1,21 @@
 class Item {
+    constructor(description, unitPrice, sequenceNumber, quantity) {
+        this.description = description;
+        this.unitPrice = unitPrice;
+        this.sequenceNumber = sequenceNumber;
+        this.quantity = quantity || 1;
+    }
+
     toXMLObject() {
-        return {};
+        return {
+            ProductItem: {
+                Description: this.description,
+                Quantity: this.quantity,
+                UnitPrice: this.unitPrice,
+                Amount: this.unitPrice * this.quantity,
+                SequenceNumber: this.sequenceNumber
+            }
+        };
     }
 }
 
