683250ec741abe78de97ddac7833404c990c6b74
Sin-Fong Lyu <kingispeak@gmail.com>
2018-01-05 14:02:01 +0800

    Add repeat function

--- src/js/common/utils.js
+++ src/js/common/utils.js
@@ -1,5 +1,13 @@
 export default {
     padZero: function(number, size) {
         return ('000000000' + number).substr(-size);
+    },
+    repeat: function(string, times) {
+        let repeatedString = '';
+        while (times > 0) {
+            repeatedString += string;
+            times--;
+        }
+        return repeatedString;
     }
 };
