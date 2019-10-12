e2e81fea71343e32975d41d9c03200398b2fdbac
Sin-Fong Lyu <kingispeak@gmail.com>
2019-10-12 16:18:04 +0800

    Simplify code

--- src/js/receipt.js
+++ src/js/receipt.js
@@ -36,9 +36,7 @@ class Receipt {
         if (!(info instanceof ReceiptInfo)) {
             throw new TypeError('Info is not ReceiptInfo class!');
         }
-        const invalidItems = items.filter(item => {
-            return !(item instanceof Item);
-        });
+        const invalidItems = items.filter(item => !(item instanceof Item));
         if (invalidItems.length > 0) {
             throw new TypeError('Some items are not Item class!');
         }
@@ -58,28 +56,22 @@ class Receipt {
     // 53925591  10510LC60122037...
     // The range of receipt number is the 15th position and 10 characters
     static parseWinnersList(winnersList) {
-        return winnersList.split(/\n|\r\n?/).map(winner => {
-            return winner.substr(15, 10);
-        });
+        return winnersList
+            .split(/\n|\r\n?/)
+            .map(winner => winner.substr(15, 10));
     }
 
     // Getters
     get taxItems() {
-        return this.items.filter(item => {
-            return item.taxType === TaxType.TAX;
-        });
+        return this.items.filter(item => item.taxType === TaxType.TAX);
     }
 
     get freeTaxItems() {
-        return this.items.filter(item => {
-            return item.taxType === TaxType.FREE_TAX;
-        });
+        return this.items.filter(item => item.taxType === TaxType.FREE_TAX);
     }
 
     get zeroTaxItems() {
-        return this.items.filter(item => {
-            return item.taxType === TaxType.ZERO_TAX;
-        });
+        return this.items.filter(item => item.taxType === TaxType.ZERO_TAX);
     }
 
     get chineseYear() {
@@ -92,9 +84,7 @@ class Receipt {
     }
 
     get totalQuantity() {
-        return this.items.reduce((sum, item) => {
-            return sum + item.quantity;
-        }, 0);
+        return this.items.reduce((sum, item) => sum + item.quantity, 0);
     }
 
     // Methods
@@ -103,9 +93,7 @@ class Receipt {
         let receiptObject = {
             $: defaultInvoiceAttr,
             Main: this.info.toXMLObject(),
-            Details: this.items.map(item => {
-                return item.toXMLObject();
-            }),
+            Details: this.items.map(item => item.toXMLObject()),
             Amounts: this.amount.toXMLObject()
         };
 
@@ -166,7 +154,7 @@ class Receipt {
                 8
             ),
             plainText = this.info.number + this.info.randomNumber,
-            padding = 16 - plainText.length % 16;
+            padding = 16 - (plainText.length % 16);
 
         plainText += utils.repeat(padding, padding);
 
@@ -276,21 +264,15 @@ class Receipt {
     }
 
     get taxItems() {
-        return this.items.filter(item => {
-            return item.taxType === TaxType.TAX;
-        });
+        return this.items.filter(item => item.taxType === TaxType.TAX);
     }
 
     get freeTaxItems() {
-        return this.items.filter(item => {
-            return item.taxType === TaxType.FREE_TAX;
-        });
+        return this.items.filter(item => item.taxType === TaxType.FREE_TAX);
     }
 
     get zeroTaxItems() {
-        return this.items.filter(item => {
-            return item.taxType === TaxType.ZERO_TAX;
-        });
+        return this.items.filter(item => item.taxType === TaxType.ZERO_TAX);
     }
 
     get chineseYear() {
@@ -303,9 +285,7 @@ class Receipt {
     }
 
     get totalQuantity() {
-        return this.items.reduce((sum, item) => {
-            return sum + item.quantity;
-        }, 0);
+        return this.items.reduce((sum, item) => sum + item.quantity, 0);
     }
 }
 
